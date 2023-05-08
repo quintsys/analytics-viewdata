98fcef09abc9be0cd5eb202d41e85ecf9e4887a2
Erich N Quintero <qbantek@gmail.com>
2023-05-07 22:15:36 -0400

    Add CORS headers
    This commit updates the functions to set CORS headers and return a 200
    status code for OPTIONS requests.
    
    Extracted an onError helper function that centralizes error handling
    logic and returns a generic error message to the client.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
-import {defineSecret} from "firebase-functions/params";
-import {google} from "googleapis";
-import {AnalyticsData, Dimensions, Row, RowFormatter} from "./types";
+import { defineSecret } from "firebase-functions/params";
+import { google } from "googleapis";
+import { AnalyticsData, Dimensions, Row, RowFormatter } from "./types";
 
 const serviceAccount = defineSecret("GA_SVC_ACCOUNT");
 
@@ -10,28 +10,43 @@ const dimensions: Dimensions = {
   origin: "ga:dimension5,ga:campaign,ga:source,ga:medium,ga:keyword",
 };
 
+const setCors = (res: functions.Response) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Max-Age', (60 * 60 * 24 * 365).toString());
+}
+
+const onError = (res: functions.Response, error: any) => {
+  functions.logger.error(error);
+  res.status(500).send({ error: "Something went wrong" });
+}
+
 /**
  * Pulls origin data from a Google Analytics view and returns it as JSON
  * @returns {AnalyticsData[]}
  */
 export const gaViewOriginData = functions
-  .runWith({secrets: [serviceAccount.name]})
+  .runWith({ secrets: [serviceAccount.name] })
   .https.onRequest(
-    async (_request, response) => {
+    async (req, res) => {
       try {
-        const formatter: RowFormatter = (row: Row) => ({
-          clientId: row[0],
-          campaign: row[1],
-          source: row[2],
-          medium: row[3],
-          keyword: row[4],
-        });
-        const data = await gaViewData(dimensions.origin, formatter);
-
-        response.status(200).send(data);
+        setCors(res);
+        if (req.method == 'OPTIONS') {
+          res.sendStatus(200);
+        } else {
+          const formatter: RowFormatter = (row: Row) => ({
+            clientId: row[0],
+            campaign: row[1],
+            source: row[2],
+            medium: row[3],
+            keyword: row[4],
+          });
+          const data = await gaViewData(dimensions.origin, formatter);
+          res.status(200).send(data);
+        }
       } catch (error) {
-        functions.logger.error(error);
-        response.status(500).send({error: "Something went wrong"});
+        onError(res, error);
       }
     }
   );
@@ -41,22 +56,25 @@ export const gaViewOriginData = functions
  * @returns {AnalyticsData[]}
  */
 export const gaViewAdData = functions
-  .runWith({secrets: [serviceAccount.name]})
+  .runWith({ secrets: [serviceAccount.name] })
   .https.onRequest(
-    async (_request, response) => {
+    async (req, res) => {
       try {
-        const formatter: RowFormatter = (row: Row) => ({
-          clientId: row[0],
-          adGroup: row[1],
-          adContent: row[2],
-          adMatchedQuery: row[3],
-        });
-        const data = await gaViewData(dimensions.ad, formatter);
-
-        response.status(200).send(data);
+        setCors(res);
+        if (req.method == 'OPTIONS') {
+          res.sendStatus(200);
+        } else {
+          const formatter: RowFormatter = (row: Row) => ({
+            clientId: row[0],
+            adGroup: row[1],
+            adContent: row[2],
+            adMatchedQuery: row[3],
+          });
+          const data = await gaViewData(dimensions.ad, formatter);
+          res.status(200).send(data);
+        }
       } catch (error) {
-        functions.logger.error(error);
-        response.status(500).send({error: "Something went wrong"});
+        onError(res, error);
       }
     }
   );
@@ -83,7 +101,7 @@ const gaViewData = async (
  * Returns Google Analytics data for a view
  * @param {string} dimensions - The dimensions to retrieve from Analytics.
  */
-async function getAnalyticsData(dimensions: string) {
+const getAnalyticsData = async (dimensions: string) => {
   const auth = new google.auth.GoogleAuth({
     credentials: JSON.parse(serviceAccount.value()),
     scopes: ["https://www.googleapis.com/auth/analytics.readonly"],
@@ -104,4 +122,3 @@ async function getAnalyticsData(dimensions: string) {
     "dimensions": dimensions,
   });
 }
-
